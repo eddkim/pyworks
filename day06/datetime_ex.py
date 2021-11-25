61bb09e8c3eadb2519a1461093ead4cdd9533032
eddkim <ghddus456@gmail.com>
2021-11-25 12:51:20 +0900

    day06

---
+++ day06/datetime_ex.py
@@ -0,0 +1,10 @@
+import datetime
+
+print(datetime.datetime.today())
+print(datetime.date.today())
+
+now = datetime.datetime.today()
+print("{}년 {}월 {}일".format(now.year, now.month, now.day))
+print("{}시 {}분 {}초".format(now.hour, now.minute, now.second))
+
+print(now.strftime("%Y.%m.%d %H:%M:%S"))
